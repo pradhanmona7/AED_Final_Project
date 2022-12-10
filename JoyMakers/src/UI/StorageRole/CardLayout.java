6cc4a2d6cdbe7b8f7393f302358810ffdad8b8f1
jadhavmrinalini98 <114711523+jadhavmrinalini98@users.noreply.github.com>
2022-12-09 23:31:57 -0500

    Storage added

---
+++ JoyMakers/src/UI/StorageRole/CardLayout.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI.StorageRole;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Mrinalini
+ */
+class CardLayout {
+
+    void previous(JPanel userProcessContainer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
